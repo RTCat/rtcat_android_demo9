745f3809c4409a7b9f6ec5f2b2dced5ef339c136
spacetime <jingweiming-sp@outlook.com>
2016-07-26 18:19:39 +0800

    fix

--- app/src/main/java/com/shishimao/bluetooth/TestConfig.java
+++ app/src/main/java/com/shishimao/bluetooth/TestConfig.java
@@ -1,16 +1,9 @@
 package com.shishimao.bluetooth;
 
-/**
- * Email : chencong@learning-tech.com
- * Created by chencong on 6/15/16.
- */
+
 public class TestConfig {
     static final String APIKEY = "91af9be9-7bac-40df-962d-f141eebe9d43";
     static final String SECRET = "ce8df259-1f7a-4aa4-9e4e-862af9f6c24d";
     static final String P2P_SESSION = "073df4a9-e9ef-4f45-a2b0-e14c778bcd86";
-    static final String RELAY_SESSION = "13129940-9127-4369-92d8-9a335d5155cc";
 
-    static final String SIGNAL_SERVER = "wss://192.168.1.24:3001";
-    static final String SIGNAL_API = "https://192.168.1.24:3004/relay/record";
-    static final String MEDIA_SERVER = "http://media.realtimecat.com";
 }
